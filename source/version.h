8ddb2ed277a84f7db4aec28920d30b8cbb4f185e
challanger2000 <challanger2000@gmx.de>
2026-09-25 01:41:41 +0200

    Set Ugly Reverb V2 version to 2.0.0

--- source/version.h
+++ source/version.h
@@ -1,12 +1,12 @@
 #pragma once
 
-#define MAJOR_VERSION_INT 0
-#define SUB_VERSION_INT 1
+#define MAJOR_VERSION_INT 2
+#define SUB_VERSION_INT 0
 #define RELEASE_NUMBER_INT 0
 #define BUILD_NUMBER_INT 0
 
-#define FULL_VERSION_STR "0.1.0"
-#define VERSION_STR "0.1.0"
+#define FULL_VERSION_STR "2.0.0"
+#define VERSION_STR "2.0.0"
 
 #define stringCompanyName "125A Audio Software"
 #define stringCompanyWeb "https://github.com/challanger2000"
